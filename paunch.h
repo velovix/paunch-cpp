07c3936e44a54d78f345404569f7d8ec5eb41f88
Tyler Compton <xaviosx@gmail.com>
2014-05-19 02:10:34 -0700

    Convert constants to enums.

--- paunch.h
+++ paunch.h
@@ -15,176 +15,192 @@
 namespace paunch {
 
 // Plane Constants
-const int X = 1;
-const int Y = 2;
+enum PLANE {
+	X = 1,
+	Y = 2,
+};
 
 // Directional Constants
-const int Up    = 1;
-const int Down  = 2;
-const int Left  = 3;
-const int Right = 4;
+enum DIRECTION {
+	Up    = 1,
+	Down  = 2,
+	Left  = 3,
+	Right = 4,
+};
 
 // Button State Constants
-const int Press   = GLFW_PRESS;
-const int Release = GLFW_RELEASE;
-const int Hold    = GLFW_REPEAT;
+enum BUTTON_STATE {
+	Press   = GLFW_PRESS,
+	Release = GLFW_RELEASE,
+	Hold    = GLFW_REPEAT,
+};
 
 // Mouse Button ID Constants
-const int MouseButton1      = GLFW_MOUSE_BUTTON_1;
-const int MouseButton2      = GLFW_MOUSE_BUTTON_2;
-const int MouseButton3      = GLFW_MOUSE_BUTTON_3;
-const int MouseButton4      = GLFW_MOUSE_BUTTON_4;
-const int MouseButton5      = GLFW_MOUSE_BUTTON_5;
-const int MouseButton6      = GLFW_MOUSE_BUTTON_6;
-const int MouseButton7      = GLFW_MOUSE_BUTTON_7;
-const int MouseButton8      = GLFW_MOUSE_BUTTON_8;
-const int MouseButtonLast   = GLFW_MOUSE_BUTTON_LAST;
-const int MouseButtonLeft   = GLFW_MOUSE_BUTTON_LEFT;
-const int MouseButtonRight  = GLFW_MOUSE_BUTTON_RIGHT;
-const int MouseButtonMiddle = GLFW_MOUSE_BUTTON_MIDDLE;
+enum MOUSE_BUTTON {
+	MouseButton1      = GLFW_MOUSE_BUTTON_1,
+	MouseButton2      = GLFW_MOUSE_BUTTON_2,
+	MouseButton3      = GLFW_MOUSE_BUTTON_3,
+	MouseButton4      = GLFW_MOUSE_BUTTON_4,
+	MouseButton5      = GLFW_MOUSE_BUTTON_5,
+	MouseButton6      = GLFW_MOUSE_BUTTON_6,
+	MouseButton7      = GLFW_MOUSE_BUTTON_7,
+	MouseButton8      = GLFW_MOUSE_BUTTON_8,
+	MouseButtonLast   = GLFW_MOUSE_BUTTON_LAST,
+	MouseButtonLeft   = GLFW_MOUSE_BUTTON_LEFT,
+	MouseButtonRight  = GLFW_MOUSE_BUTTON_RIGHT,
+	MouseButtonMiddle = GLFW_MOUSE_BUTTON_MIDDLE,
+};
 
 // Keyboard Key ID Constants
-const int KeyUnknown      = GLFW_KEY_UNKNOWN;
-const int KeySpace        = GLFW_KEY_SPACE;
-const int KeyApostrophe   = GLFW_KEY_APOSTROPHE;
-const int KeyComma        = GLFW_KEY_COMMA;
-const int KeyMinus        = GLFW_KEY_MINUS;
-const int KeyPeriod       = GLFW_KEY_PERIOD;
-const int KeySlash        = GLFW_KEY_SLASH;
-const int Key0            = GLFW_KEY_0;
-const int Key1            = GLFW_KEY_1;
-const int Key2            = GLFW_KEY_2;
-const int Key3            = GLFW_KEY_3;
-const int Key4            = GLFW_KEY_4;
-const int Key5            = GLFW_KEY_5;
-const int Key6            = GLFW_KEY_6;
-const int Key7            = GLFW_KEY_7;
-const int Key8            = GLFW_KEY_8;
-const int Key9            = GLFW_KEY_9;
-const int KeySemicolon    = GLFW_KEY_SEMICOLON;
-const int KeyEqual        = GLFW_KEY_EQUAL;
-const int KeyA            = GLFW_KEY_A;
-const int KeyB            = GLFW_KEY_B;
-const int KeyC            = GLFW_KEY_C;
-const int KeyD            = GLFW_KEY_D;
-const int KeyE            = GLFW_KEY_E;
-const int KeyF            = GLFW_KEY_F;
-const int KeyG            = GLFW_KEY_G;
-const int KeyH            = GLFW_KEY_H;
-const int KeyI            = GLFW_KEY_I;
-const int KeyJ            = GLFW_KEY_J;
-const int KeyK            = GLFW_KEY_K;
-const int KeyL            = GLFW_KEY_L;
-const int KeyM            = GLFW_KEY_M;
-const int KeyN            = GLFW_KEY_N;
-const int KeyO            = GLFW_KEY_O;
-const int KeyP            = GLFW_KEY_P;
-const int KeyQ            = GLFW_KEY_Q;
-const int KeyR            = GLFW_KEY_R;
-const int KeyS            = GLFW_KEY_S;
-const int KeyT            = GLFW_KEY_T;
-const int KeyU            = GLFW_KEY_U;
-const int KeyV            = GLFW_KEY_V;
-const int KeyW            = GLFW_KEY_W;
-const int KeyX            = GLFW_KEY_X;
-const int KeyY            = GLFW_KEY_Y;
-const int KeyZ            = GLFW_KEY_Z;
-const int KeyLeftBracket  = GLFW_KEY_LEFT_BRACKET;
-const int KeyBackslash    = GLFW_KEY_BACKSLASH;
-const int KeyRightBracket = GLFW_KEY_RIGHT_BRACKET;
-const int KeyGraveAccent  = GLFW_KEY_GRAVE_ACCENT;
-const int KeyWorld1       = GLFW_KEY_WORLD_1;
-const int KeyWorld2       = GLFW_KEY_WORLD_2;
-const int KeyEscape       = GLFW_KEY_ESCAPE;
-const int KeyEnter        = GLFW_KEY_ENTER;
-const int KeyTab          = GLFW_KEY_TAB;
-const int KeyBackspace    = GLFW_KEY_BACKSPACE;
-const int KeyInsert       = GLFW_KEY_INSERT;
-const int KeyDelete       = GLFW_KEY_DELETE;
-const int KeyRight        = GLFW_KEY_RIGHT;
-const int KeyLeft         = GLFW_KEY_LEFT;
-const int KeyDown         = GLFW_KEY_DOWN;
-const int KeyUp           = GLFW_KEY_UP;
-const int KeyPageUp       = GLFW_KEY_PAGE_UP;
-const int KeyPageDown     = GLFW_KEY_PAGE_DOWN;
-const int KeyHome         = GLFW_KEY_HOME;
-const int KeyEnd          = GLFW_KEY_END;
-const int KeyCapsLock     = GLFW_KEY_CAPS_LOCK;
-const int KeyScrollLock   = GLFW_KEY_SCROLL_LOCK;
-const int KeyNumLock      = GLFW_KEY_NUM_LOCK;
-const int KeyPrintScreen  = GLFW_KEY_PRINT_SCREEN;
-const int KeyPause        = GLFW_KEY_PAUSE;
-const int KeyF1           = GLFW_KEY_F1;
-const int KeyF2           = GLFW_KEY_F2;
-const int KeyF3           = GLFW_KEY_F3;
-const int KeyF4           = GLFW_KEY_F4;
-const int KeyF5           = GLFW_KEY_F5;
-const int KeyF6           = GLFW_KEY_F6;
-const int KeyF7           = GLFW_KEY_F7;
-const int KeyF8           = GLFW_KEY_F8;
-const int KeyF9           = GLFW_KEY_F9;
-const int KeyF10          = GLFW_KEY_F10;
-const int KeyF11          = GLFW_KEY_F11;
-const int KeyF12          = GLFW_KEY_F12;
-const int KeyF13          = GLFW_KEY_F13;
-const int KeyF14          = GLFW_KEY_F14;
-const int KeyF15          = GLFW_KEY_F15;
-const int KeyF16          = GLFW_KEY_F16;
-const int KeyF17          = GLFW_KEY_F17;
-const int KeyF18          = GLFW_KEY_F18;
-const int KeyF19          = GLFW_KEY_F19;
-const int KeyF20          = GLFW_KEY_F20;
-const int KeyF21          = GLFW_KEY_F21;
-const int KeyF22          = GLFW_KEY_F22;
-const int KeyF23          = GLFW_KEY_F23;
-const int KeyF24          = GLFW_KEY_F24;
-const int KeyF25          = GLFW_KEY_F25;
-const int KeyKp0          = GLFW_KEY_KP_0;
-const int KeyKp1          = GLFW_KEY_KP_1;
-const int KeyKp2          = GLFW_KEY_KP_2;
-const int KeyKp3          = GLFW_KEY_KP_3;
-const int KeyKp4          = GLFW_KEY_KP_4;
-const int KeyKp5          = GLFW_KEY_KP_5;
-const int KeyKp6          = GLFW_KEY_KP_6;
-const int KeyKp7          = GLFW_KEY_KP_7;
-const int KeyKp8          = GLFW_KEY_KP_8;
-const int KeyKp9          = GLFW_KEY_KP_9;
-const int KeyKpDecimal    = GLFW_KEY_KP_DECIMAL;
-const int KeyKpDivide     = GLFW_KEY_KP_DIVIDE;
-const int KeyKpMultiply   = GLFW_KEY_KP_MULTIPLY;
-const int KeyKpSubtract   = GLFW_KEY_KP_SUBTRACT;
-const int KeyKpAdd        = GLFW_KEY_KP_ADD;
-const int KeyKpEnter      = GLFW_KEY_KP_ENTER;
-const int KeyKpEqual      = GLFW_KEY_KP_EQUAL;
-const int KeyLeftShift    = GLFW_KEY_LEFT_SHIFT;
-const int KeyLeftControl  = GLFW_KEY_LEFT_CONTROL;
-const int KeyLeftAlt      = GLFW_KEY_LEFT_ALT;
-const int KeyLeftSuper    = GLFW_KEY_LEFT_SUPER;
-const int KeyRightShift   = GLFW_KEY_RIGHT_SHIFT;
-const int KeyRightControl = GLFW_KEY_RIGHT_CONTROL;
-const int KeyRightAlt     = GLFW_KEY_RIGHT_ALT;
-const int KeyRightSuper   = GLFW_KEY_RIGHT_SUPER;
-const int KeyMenu         = GLFW_KEY_MENU;
-const int KeyLast         = GLFW_KEY_LAST;
+enum KEYBOARD_KEY {
+	KeyUnknown      = GLFW_KEY_UNKNOWN,
+	KeySpace        = GLFW_KEY_SPACE,
+	KeyApostrophe   = GLFW_KEY_APOSTROPHE,
+	KeyComma        = GLFW_KEY_COMMA,
+	KeyMinus        = GLFW_KEY_MINUS,
+	KeyPeriod       = GLFW_KEY_PERIOD,
+	KeySlash        = GLFW_KEY_SLASH,
+	Key0            = GLFW_KEY_0,
+	Key1            = GLFW_KEY_1,
+	Key2            = GLFW_KEY_2,
+	Key3            = GLFW_KEY_3,
+	Key4            = GLFW_KEY_4,
+	Key5            = GLFW_KEY_5,
+	Key6            = GLFW_KEY_6,
+	Key7            = GLFW_KEY_7,
+	Key8            = GLFW_KEY_8,
+	Key9            = GLFW_KEY_9,
+	KeySemicolon    = GLFW_KEY_SEMICOLON,
+	KeyEqual        = GLFW_KEY_EQUAL,
+	KeyA            = GLFW_KEY_A,
+	KeyB            = GLFW_KEY_B,
+	KeyC            = GLFW_KEY_C,
+	KeyD            = GLFW_KEY_D,
+	KeyE            = GLFW_KEY_E,
+	KeyF            = GLFW_KEY_F,
+	KeyG            = GLFW_KEY_G,
+	KeyH            = GLFW_KEY_H,
+	KeyI            = GLFW_KEY_I,
+	KeyJ            = GLFW_KEY_J,
+	KeyK            = GLFW_KEY_K,
+	KeyL            = GLFW_KEY_L,
+	KeyM            = GLFW_KEY_M,
+	KeyN            = GLFW_KEY_N,
+	KeyO            = GLFW_KEY_O,
+	KeyP            = GLFW_KEY_P,
+	KeyQ            = GLFW_KEY_Q,
+	KeyR            = GLFW_KEY_R,
+	KeyS            = GLFW_KEY_S,
+	KeyT            = GLFW_KEY_T,
+	KeyU            = GLFW_KEY_U,
+	KeyV            = GLFW_KEY_V,
+	KeyW            = GLFW_KEY_W,
+	KeyX            = GLFW_KEY_X,
+	KeyY            = GLFW_KEY_Y,
+	KeyZ            = GLFW_KEY_Z,
+	KeyLeftBracket  = GLFW_KEY_LEFT_BRACKET,
+	KeyBackslash    = GLFW_KEY_BACKSLASH,
+	KeyRightBracket = GLFW_KEY_RIGHT_BRACKET,
+	KeyGraveAccent  = GLFW_KEY_GRAVE_ACCENT,
+	KeyWorld1       = GLFW_KEY_WORLD_1,
+	KeyWorld2       = GLFW_KEY_WORLD_2,
+	KeyEscape       = GLFW_KEY_ESCAPE,
+	KeyEnter        = GLFW_KEY_ENTER,
+	KeyTab          = GLFW_KEY_TAB,
+	KeyBackspace    = GLFW_KEY_BACKSPACE,
+	KeyInsert       = GLFW_KEY_INSERT,
+	KeyDelete       = GLFW_KEY_DELETE,
+	KeyRight        = GLFW_KEY_RIGHT,
+	KeyLeft         = GLFW_KEY_LEFT,
+	KeyDown         = GLFW_KEY_DOWN,
+	KeyUp           = GLFW_KEY_UP,
+	KeyPageUp       = GLFW_KEY_PAGE_UP,
+	KeyPageDown     = GLFW_KEY_PAGE_DOWN,
+	KeyHome         = GLFW_KEY_HOME,
+	KeyEnd          = GLFW_KEY_END,
+	KeyCapsLock     = GLFW_KEY_CAPS_LOCK,
+	KeyScrollLock   = GLFW_KEY_SCROLL_LOCK,
+	KeyNumLock      = GLFW_KEY_NUM_LOCK,
+	KeyPrintScreen  = GLFW_KEY_PRINT_SCREEN,
+	KeyPause        = GLFW_KEY_PAUSE,
+	KeyF1           = GLFW_KEY_F1,
+	KeyF2           = GLFW_KEY_F2,
+	KeyF3           = GLFW_KEY_F3,
+	KeyF4           = GLFW_KEY_F4,
+	KeyF5           = GLFW_KEY_F5,
+	KeyF6           = GLFW_KEY_F6,
+	KeyF7           = GLFW_KEY_F7,
+	KeyF8           = GLFW_KEY_F8,
+	KeyF9           = GLFW_KEY_F9,
+	KeyF10          = GLFW_KEY_F10,
+	KeyF11          = GLFW_KEY_F11,
+	KeyF12          = GLFW_KEY_F12,
+	KeyF13          = GLFW_KEY_F13,
+	KeyF14          = GLFW_KEY_F14,
+	KeyF15          = GLFW_KEY_F15,
+	KeyF16          = GLFW_KEY_F16,
+	KeyF17          = GLFW_KEY_F17,
+	KeyF18          = GLFW_KEY_F18,
+	KeyF19          = GLFW_KEY_F19,
+	KeyF20          = GLFW_KEY_F20,
+	KeyF21          = GLFW_KEY_F21,
+	KeyF22          = GLFW_KEY_F22,
+	KeyF23          = GLFW_KEY_F23,
+	KeyF24          = GLFW_KEY_F24,
+	KeyF25          = GLFW_KEY_F25,
+	KeyKp0          = GLFW_KEY_KP_0,
+	KeyKp1          = GLFW_KEY_KP_1,
+	KeyKp2          = GLFW_KEY_KP_2,
+	KeyKp3          = GLFW_KEY_KP_3,
+	KeyKp4          = GLFW_KEY_KP_4,
+	KeyKp5          = GLFW_KEY_KP_5,
+	KeyKp6          = GLFW_KEY_KP_6,
+	KeyKp7          = GLFW_KEY_KP_7,
+	KeyKp8          = GLFW_KEY_KP_8,
+	KeyKp9          = GLFW_KEY_KP_9,
+	KeyKpDecimal    = GLFW_KEY_KP_DECIMAL,
+	KeyKpDivide     = GLFW_KEY_KP_DIVIDE,
+	KeyKpMultiply   = GLFW_KEY_KP_MULTIPLY,
+	KeyKpSubtract   = GLFW_KEY_KP_SUBTRACT,
+	KeyKpAdd        = GLFW_KEY_KP_ADD,
+	KeyKpEnter      = GLFW_KEY_KP_ENTER,
+	KeyKpEqual      = GLFW_KEY_KP_EQUAL,
+	KeyLeftShift    = GLFW_KEY_LEFT_SHIFT,
+	KeyLeftControl  = GLFW_KEY_LEFT_CONTROL,
+	KeyLeftAlt      = GLFW_KEY_LEFT_ALT,
+	KeyLeftSuper    = GLFW_KEY_LEFT_SUPER,
+	KeyRightShift   = GLFW_KEY_RIGHT_SHIFT,
+	KeyRightControl = GLFW_KEY_RIGHT_CONTROL,
+	KeyRightAlt     = GLFW_KEY_RIGHT_ALT,
+	KeyRightSuper   = GLFW_KEY_RIGHT_SUPER,
+	KeyMenu         = GLFW_KEY_MENU,
+	KeyLast         = GLFW_KEY_LAST,
+};
 
 // Shader Type Constants
-const int Vertex   = GL_VERTEX_SHADER;
-const int Fragment = GL_FRAGMENT_SHADER;
+enum SHADER_TYPE {
+	Vertex   = GL_VERTEX_SHADER,
+	Fragment = GL_FRAGMENT_SHADER,
+};
 
 // Vertex Interpretation Constants
-const GLuint Points        = GL_POINTS;
-const GLuint LineStrip     = GL_LINE_STRIP;
-const GLuint LineLoop      = GL_LINE_LOOP;
-const GLuint Lines         = GL_LINES;
-const GLuint TriangleStrip = GL_TRIANGLE_STRIP;
-const GLuint TriangleFan   = GL_TRIANGLE_FAN;
-const GLuint Triangles     = GL_TRIANGLES;
+enum VERTEX_SHAPE {
+	Points        = GL_POINTS,
+	LineStrip     = GL_LINE_STRIP,
+	LineLoop      = GL_LINE_LOOP,
+	Lines         = GL_LINES,
+	TriangleStrip = GL_TRIANGLE_STRIP,
+	TriangleFan   = GL_TRIANGLE_FAN,
+	Triangles     = GL_TRIANGLES,
+};
 
 // Sound State Constants
-const int Initial = AL_INITIAL;
-const int Playing = AL_PLAYING;
-const int Paused  = AL_PAUSED;
-const int Stopped = AL_STOPPED;
+enum SOUND_STATE {
+	Initial = AL_INITIAL,
+	Playing = AL_PLAYING,
+	Paused  = AL_PAUSED,
+	Stopped = AL_STOPPED,
+};
 
 /* class Error
  * Error defines the exception type that is thrown by Paunch-specific
